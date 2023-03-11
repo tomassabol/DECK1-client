a25ff393cdf14023102a12f1a253da3f671fa6ca
Tomáš Sabol <tomassabol2@gmail.com>
2023-03-11 17:31:33 +0100

    router setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './assets/sass/style.scss'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia"; // https://pinia.vuejs.org/getting-started.html#installation
+import router from "./router";
+import "./assets/sass/style.scss";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia()).use(router).mount("#app");
---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+// Docs: https://router.vuejs.org/
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: "/",
+        name: "home",
+        component: () => import("../views/HomeView.vue"),
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
