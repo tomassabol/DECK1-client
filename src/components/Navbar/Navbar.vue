71cdedf0b3ff68de505f0bc12e52926aabc03527
Tomáš Sabol <tomassabol2@gmail.com>
2023-03-12 19:01:30 +0100

    navbar styling & logic

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div
+        class="flex w-80 bg-[#F5F5F5] h-screen flex-col justify-between items-center"
+    >
+        <router-link :to="{ name: 'Dashboard' }" class="mt-10">
+            <img
+                :src="`/images/deck1-logo.svg`"
+                class="initial loading w-64"
+                data-was-processed="true"
+            />
+        </router-link>
+        <ol class="flex flex-col gap-1.5 font-bold text-2xl">
+            <div v-for="option in menuOptions">
+                <NavbarOption :currentRoute="currentRoute" :option="option" />
+            </div>
+        </ol>
+        <router-link :to="{ name: 'Dashboard' }">
+            <div class="flex gap-2 items-center text-2xl mb-14 font-bold">
+                <i class="fa-solid fa-right-from-bracket rotate-180" />
+                <span>Log Out</span>
+            </div>
+        </router-link>
+    </div>
+</template>
+<script setup lang="ts">
+import { computed, ComputedRef } from "vue";
+import { useRouter } from "vue-router";
+import NavbarOption from "./NavbarOption.vue";
+
+import.meta.glob("../../public/images/deck1-logo.svg", {
+    as: "raw",
+});
+
+const router = useRouter();
+
+const menuOptions: string[] = [
+    "Dashboard",
+    "Daily Reports",
+    "Daily Updates",
+    "Sites",
+    "Profile",
+];
+
+const currentRoute: ComputedRef<string> = computed(() => {
+    return router.currentRoute.value.name as string;
+});
+</script>
---
+++ src/components/Navbar/NavbarOption.vue
@@ -0,0 +1,33 @@
+<template>
+    <router-link
+        v-if="option.replaceAll(' ', '') === currentRoute"
+        class="flex bg-[#222] text-white items-center rounded-xl p-1 px-3"
+        :to="{ name: option.replaceAll(' ', '') }"
+    >
+        <div class="flex items-center px-1.5">
+            <img
+                :src="`/icons/${option.replaceAll(' ', '')}.png`"
+                alt=""
+                class="w-10 p-2"
+            />
+            <span class="mr-4">{{ option }}</span>
+        </div>
+    </router-link>
+
+    <router-link v-else :to="{ name: option.replaceAll(' ', '') }">
+        <div class="flex items-center px-1.5">
+            <img
+                :src="`/icons/${option.replaceAll(' ', '')}.png`"
+                alt=""
+                class="w-10 p-2"
+            />
+            <span class="mr-4">{{ option }}</span>
+        </div>
+    </router-link>
+</template>
+<script setup lang="ts">
+defineProps<{
+    option: string;
+    currentRoute: any;
+}>();
+</script>
