ba4af25eaffd017824dc1741a6624e1661cfd16d
Tomáš Sabol <tomassabol2@gmail.com>
2023-04-06 22:05:31 +0200

    graphql setup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex">
-        <Navbar />
-        <RouterView></RouterView>
-    </div>
+  <div class="flex">
+    <Navbar />
+    <RouterView></RouterView>
+  </div>
 </template>
 
 <script setup lang="ts">
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,37 @@
-import { createApp } from "vue";
+import { createApp, h, provide } from "vue";
 import { createPinia } from "pinia"; // https://pinia.vuejs.org/getting-started.html#installation
 import router from "./router";
 import "./index.scss";
 import App from "./App.vue";
+// graphql
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: "http://localhost:4000/graphql",
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+  render: () => h(App),
+});
+
+app.use(createPinia()).use(router).mount("#app");
+// createApp(App).use(createPinia()).use(router).use(VueApollo).mount("#app");
