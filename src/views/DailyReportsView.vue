08849535248f07982c5bcc9b1782f6c4eb3daedd
Tomáš Sabol <tomassabol2@gmail.com>
2023-04-08 20:42:07 +0200

    fixed deployment

--- src/views/DailyReportsView.vue
+++ src/views/DailyReportsView.vue
@@ -2,7 +2,11 @@
   <div class="flex flex-col m-14 gap-12 w-full">
     <PageTitle :primaryText="title" />
     <div class="flex items-end justify-end">
-      <ButtonReusable text="New Report" @click="navigate" />
+      <ButtonReusable
+        text="New Report"
+        @click="navigate"
+        :loading="isLoading"
+      />
     </div>
     <Table
       :tableHeaders="tableHeaders"
@@ -16,10 +20,14 @@ import PageTitle from "@/components/Headers/PageTitle.vue";
 import ButtonReusable from "@/components/Buttons/ButtonReusable.vue";
 import Table from "@/components/Tables/TableReusable.vue";
 import { useRouter } from "vue-router";
+import { useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+import { computed, ref, watch } from "vue";
 
 const router = useRouter();
 
 const title = "Daily Reports";
+const isLoading = ref(false);
 
 // TODO: retrieve data from api
 const tableHeaders: Types.TableHeader = {
@@ -31,26 +39,34 @@ const tableHeaders: Types.TableHeader = {
   hoistOperator: "Hoist Operator",
   dailyUpdate: "Daily Update",
 };
-const tableData: Types.Report[] = [
-  {
-    id: "123",
-    acModel: "EC 145",
-    acRegistration: "D-HTMK",
-    date: new Date("2023-02-14"),
-    pilot: "SRO",
-    hoistOperator: "BRA",
-    dailyUpdate: true,
-  },
-  {
-    id: "124",
-    acModel: "EC 145",
-    acRegistration: "D-HTMK",
-    date: new Date("2023-02-14"),
-    pilot: "SRO",
-    hoistOperator: "BRA",
-    dailyUpdate: true,
-  },
-];
+const tableData: Types.Report[] = [];
+
+// graphql query
+const ALL_REPORTS_QUERY = gql`
+  query {
+    dailyReports {
+      id
+      helicopter {
+        model
+        reg
+      }
+      date
+      pilot {
+        name
+      }
+      hoistOperator {
+        name
+      }
+    }
+  }
+`;
+
+const { result } = useQuery(ALL_REPORTS_QUERY);
+const dailyReports = computed(() => result?.value?.dailyReports ?? []);
+
+watch(dailyReports, () => {
+  console.log(dailyReports.value);
+});
 
 function navigate() {
   router.push({ name: "NewDailyReport" });
